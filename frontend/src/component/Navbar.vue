e8b40256042ed7210afd8c74121c20e14a6e097c
hamzaavsar <hamzaavsar@degerse.com>
2025-02-19 14:24:44 +0300

    updated design of the profile page

--- frontend/src/component/Navbar.vue
+++ frontend/src/component/Navbar.vue
@@ -1,13 +1,25 @@
 <script setup>
+import {useAuthStore} from "@/store/auth.js";
+
+const authStore = useAuthStore();
+
+const logout = async () => {
+  await authStore.logout();
+}
 
 </script>
 
 <template>
   <nav class="bg-gradient-to-r from-orange-50 to-pink-100 text-white shadow-lg p-5 flex justify-between fixed top-0 w-screen ">
     <router-link to="/" class="text-black font-bold text-2xl">Yemeğim</router-link>
-    <div class="flex space-x-4">
+    <div class="flex space-x-4" v-if="!authStore.isAuthenticated">
       <router-link to="/login" class="border border-black rounded px-4 py-2 text-black hover:bg-white hover:text-orange-500 hover:border-white transition">Giriş Yap</router-link>
       <router-link to="/register" class="bg-white text-orange-500 border border-white rounded px-4 py-2 hover:bg-orange-600 hover:text-white transition">Kayıt Ol</router-link>
+    </div>
+    <div v-else class="flex space-x-4">
+      <router-link to="/profile" class="capitalize border border-black rounded px-4 py-2 text-black hover:bg-white hover:text-orange-500 hover:border-white transition">
+        {{ authStore.user?.name }}</router-link>
+      <button @click="logout" class="bg-white text-orange-500 border border-white rounded px-4 py-2 hover:bg-orange-600 hover:text-white transition">Çıkış Yap</button>
     </div>
   </nav>
 </template>
--- frontend/src/views/Profile.vue
+++ frontend/src/views/Profile.vue
@@ -1,6 +1,6 @@
 <script setup>
 
-import { useRouter } from "vue-router";
+import {useRouter} from "vue-router";
 import {useAuthStore} from "@/store/auth.js";
 import {onMounted} from "vue";
 
@@ -10,18 +10,35 @@ const router = useRouter();
 onMounted(async () => {
   await authStore.fetchUser();
 });
-const handleLogout = async () => {
-  await authStore.logout();
-  router.push("/login");
-};
+
 </script>
 
 <template>
-  <div v-if="authStore.isAuthenticated">
-    <h2>Merhaba, {{ authStore.user?.surname }}</h2>
-    <p>Email: {{ authStore.user?.email }}</p>
-    <p>Telefon: {{ authStore.user?.phone }}</p>
-    <button @click="handleLogout">Çıkış Yap</button>
+  <div v-if="authStore.isAuthenticated" class="bg-white w-4/5 rounded mt-16">
+    <div class="my-16">
+      <p class="text-3xl font-bold text-center capitalize mb-2">Merhaba, {{ authStore.user?.name }}
+        {{ authStore.user?.surname }}</p>
+      <p class="text-center text-sm font-medium">Email: {{ authStore.user?.email }}</p>
+    </div>
+    <div>
+      <div class="bg-gray-100 mx-10 my-5 p-5 rounded-2xl">
+        <p class="text-xs">Ad</p>
+        <p class="text-lg font-bold capitalize">{{ authStore.user?.name }}</p>
+      </div>
+      <div class="bg-gray-100 mx-10 my-5 p-5 rounded-2xl">
+        <p class="text-xs">Soyad</p>
+        <p class="text-lg font-bold capitalize">{{ authStore.user?.surname }}</p>
+      </div>
+      <div class="bg-gray-100 mx-10 my-5 p-5 rounded-2xl">
+        <p class="text-xs">E-posta</p>
+        <p class="text-lg font-bold">{{ authStore.user?.email }}</p>
+      </div>
+      <div class="bg-gray-100 mx-10 my-5 p-5 rounded-2xl">
+        <p class="text-xs">Telefon Numarası </p>
+        <p class="text-lg font-bold">{{ authStore.user?.phone }}</p>
+      </div>
+    </div>
+
   </div>
   <div v-else>
     <p>Lütfen giriş yapınız.</p>
