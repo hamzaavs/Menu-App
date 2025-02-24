4d4b87c532164db344d65403f94486bf972705f4
hamzaavsar <hamzaavsar@degerse.com>
2025-02-24 15:04:01 +0300

    Edited DefaultLayout

--- frontend/src/layout/DefaultLayout.vue
+++ frontend/src/layout/DefaultLayout.vue
@@ -3,9 +3,13 @@ import Navbar from "@/component/Navbar.vue";
 </script>
 
 <template>
-  <div class="w-screen h-screen bg-gray-100 flex justify-center items-center font-sans">
-    <Navbar />
-    <slot></slot>
+  <div class="flex flex-col max-w-screen h-screen bg-gray-100 justify-between">
+    <div>
+      <Navbar/>
+      <div class="bg-gray-100 flex flex-col justify-center items-center mt-16 pb-10">
+        <slot></slot>
+      </div>
+    </div>
   </div>
 </template>
 
